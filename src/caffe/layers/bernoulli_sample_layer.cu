dd6f290a7c70308c2de99d8dc80fb7f240352b15
Calvin Seward <calvin.seward@zalando.de>
2016-02-17 15:15:27 +0100

    fixed an embarasing bug here

--- src/caffe/layers/bernoulli_sample_layer.cu
+++ src/caffe/layers/bernoulli_sample_layer.cu
@@ -17,8 +17,8 @@ __global__ void CompareKernel(const int n, const Dtype* input_data,
 template <typename Dtype>
 void BernoulliSampleLayer<Dtype>::Forward_gpu(
     const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
-  const Dtype* bottom_data = bottom[0]->cpu_data();
-  Dtype* top_data = top[0]->mutable_cpu_data();
+  const Dtype* bottom_data = bottom[0]->gpu_data();
+  Dtype* top_data = top[0]->mutable_gpu_data();
   Dtype* uniform_sample = static_cast<Dtype*>(rng_data_->mutable_gpu_data());
   const int count = bottom[0]->count();
   caffe_gpu_rng_uniform<Dtype>(count, 0., 1., uniform_sample);
